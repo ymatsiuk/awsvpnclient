a67649f5bbd1b72e47d2d5c7ca2f5c5edffe75bb
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-01-03 13:59:02 +0100

    Add openvpn v2.6.8 patch

--- openvpn.nix
+++ openvpn.nix
@@ -5,8 +5,8 @@
 openvpn.overrideAttrs (oldAttrs: rec {
   patches = [
     (fetchpatch {
-      url = "https://raw.githubusercontent.com/samm-git/aws-vpn-client/master/openvpn-v2.5.1-aws.patch";
-      sha256 = "sha256-9ijhANqqWXVPa00RBCRACtMIsjiBqYVa91V62L4mNas=";
+      url = "https://raw.githubusercontent.com/ymatsiuk/aws-vpn-client/f929e910e3d81928a845f9c69f2edba958344ec6/openvpn-v2.6.8-aws.patch";
+      sha256 = "sha256-pbgmt5o/0k4lZ/mZobl0lgg39kxEASpk5hf6ndopayY=";
     })
   ];
 })
